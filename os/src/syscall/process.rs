913dc52d31cdc954ab56df905623c9f78247c726
ysyx_22051213-Zhang Yuchi <inchinaxiaofeng@gmail.com>
2024-10-29 23:20:53 +0800

    Update

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,4 +1,10 @@
 //! Process management syscalls
+use core::ptr::from_raw_parts;
+
+use crate::task::{self, current_user_token}:current_user_token;
+use crate::mm::translated_byte_buffer;
+
+use crate::timer::get_time_us;
 use crate::{
     config::MAX_SYSCALL_NUM,
     task::{
@@ -40,16 +46,36 @@ pub fn sys_yield() -> isize {
 
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
-/// HINT: What if [`TimeVal`] is splitted by two pages ?
+/// HINT: What if [`TimeVal`] is splitted by two pages ? 
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+    let us = get_time_us();
+    let dst_vec = translated_byte_buffer(
+        current_user_token(),
+        _ts as *const u8, core::mem::size_of::<TimeVal>()
+    );
+    let ref time_val = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+    let src_ptr = time_val as *const TimeVal;
+    for (idx, dst) in dst_vec.into_iter().enumerate() {
+        let unit_len = dst.len();
+        unsafe {
+            dst.copy_from_slice(from_raw_parts(
+                src_ptr.wrapping_byte_add(idx * unit_len) as *const u8,
+                unit_len)
+            );
+        }
+    }
     trace!("kernel: sys_get_time");
-    -1
+    0
 }
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TaskInfo`] is splitted by two pages ?
 pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
+
     trace!("kernel: sys_task_info NOT IMPLEMENTED YET!");
     -1
 }
